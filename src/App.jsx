389f4f1f905d12fe9d63273f66f53a303298798b
maziz07 <mazizyr@gmail.com>
2024-04-21 19:00:57 +1000

    created main ui

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
       <Hero />
       <Generator />
       <Workout />
-    
-    </>
+    </main>
   )
 }
 
