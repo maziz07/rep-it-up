40de21e10e74d6f2956d547447090e560404d39c
maziz07 <mazizyr@gmail.com>
2024-04-26 14:58:05 +1000

    updated hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,19 @@
+
 import React from 'react'
+import Button from './Button'
 
 export default function Hero() {
-  return (
-    <div className='min-h-screen flex flex-col gap=1- items-center 
-    justify-center text-center max-w-[800px]
-    w-full mx-auto p-4'>
-      <div className='flex flex-col gap-4'>
+    return (
+        <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
+            <div className='flex flex-col gap-4'>
 
-      <p> IT'S TIME TO GET</p>
-      <h1 className='uppercase font-semibold text-4xl sm:text-5xl 
-      md:text-6xl lg:text-7xl'>Fit<span className='text-blue-400'>Big</span></h1>
-      </div>
-      <p className='text-sm md:text-base font-light'>I hereby acknowledge that I may become extremely <span className='text-blue-400 font-medium'>fit and big</span>.
-      I accept all risks of becoming the <span className='text-blue-400 font-medium'>best version</span> of myself,
-      afflicted with severe body dismorphia, unable to blend in with the crowd.
-      </p>
-
-      <button className='px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
-        <p>Accept & Begin</p>
-        </button>
-    </div>
-  )
+                <p>IT'S TIME TO GET</p>
+                <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Swole<span className='text-blue-400'>normous</span></h1>
+            </div>
+            <p className='text-sm md:text-base font-light'>I hereby acknowledgement that I may become <span className='text-blue-400 font-medium'>unbelievably swolenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font-medium'>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+            <Button func={() => {
+                window.location.href = '#generate'
+            }} text={"Accept & Begin"}></Button>
+        </div>
+    )
 }
